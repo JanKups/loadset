1ad5c4672cc483223c5d679ce60f97ac48dc722b
JanKups <Jan@Kups.nl>
2016-01-29 12:13:16 +0100

    Changes columns row

--- script.js
+++ script.js
@@ -4,9 +4,9 @@
   controller('largedatacontroller', ['$scope', function($scope){
       $scope.framework = 'ReactJs';
       $scope.data = [];
-      for(var i = 0; i < 1500; ++i) {
+      for(var i = 0; i < 500; ++i) {
           $scope.data[i] = {};
-          for(var j = 0; j < 12; ++j) {
+          for(var j = 0; j < 35; ++j) {
               $scope.data[i][j] = Math.random();
           }
       }
