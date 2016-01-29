eafbc8d17c570e25e3bcec78f5caba0b265cee96
JanKups <Jan@Kups.nl>
2016-01-29 12:13:55 +0100

    Edited columns

--- datalists.js
+++ datalists.js
@@ -21,28 +21,28 @@ var DATALIST = React.createClass({displayName: 'DATALIST',
                     React.DOM.td(null, datum['8']),
                     React.DOM.td(null, datum['9']),
                     React.DOM.td(null, datum['10']),
-                    React.DOM.td(null, datum['12'])
-                    React.DOM.td(null, datum['13'])
-                    React.DOM.td(null, datum['14'])
-                    React.DOM.td(null, datum['15'])
-                    React.DOM.td(null, datum['16'])
-                    React.DOM.td(null, datum['17'])
-                    React.DOM.td(null, datum['18'])
-                    React.DOM.td(null, datum['19'])
-                    React.DOM.td(null, datum['20'])
-                    React.DOM.td(null, datum['21'])
-                    React.DOM.td(null, datum['22'])
-                    React.DOM.td(null, datum['23'])
-                    React.DOM.td(null, datum['24'])
-                    React.DOM.td(null, datum['25'])
-                    React.DOM.td(null, datum['26'])
-                    React.DOM.td(null, datum['27'])
-                    React.DOM.td(null, datum['28'])
-                    React.DOM.td(null, datum['29'])
-                    React.DOM.td(null, datum['30'])
-                    React.DOM.td(null, datum['31'])
-                    React.DOM.td(null, datum['32'])
-                    React.DOM.td(null, datum['33'])
+                    React.DOM.td(null, datum['12']),
+                    React.DOM.td(null, datum['13']),
+                    React.DOM.td(null, datum['14']),
+                    React.DOM.td(null, datum['15']),
+                    React.DOM.td(null, datum['16']),
+                    React.DOM.td(null, datum['17']),
+                    React.DOM.td(null, datum['18']),
+                    React.DOM.td(null, datum['19']),
+                    React.DOM.td(null, datum['20']),
+                    React.DOM.td(null, datum['21']),
+                    React.DOM.td(null, datum['22']),
+                    React.DOM.td(null, datum['23']),
+                    React.DOM.td(null, datum['24']),
+                    React.DOM.td(null, datum['25']),
+                    React.DOM.td(null, datum['26']),
+                    React.DOM.td(null, datum['27']),
+                    React.DOM.td(null, datum['28']),
+                    React.DOM.td(null, datum['29']),
+                    React.DOM.td(null, datum['30']),
+                    React.DOM.td(null, datum['31']),
+                    React.DOM.td(null, datum['32']),
+                    React.DOM.td(null, datum['33']),
                     React.DOM.td(null, datum['34'])
                   )
                 );
